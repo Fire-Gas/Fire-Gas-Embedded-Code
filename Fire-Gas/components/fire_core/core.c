f52e6e8fa0b17e1f8d4199457b14e42c1b57580f
ilfpns <yeomsh09@gmail.com>
2026-04-17 23:17:59 +0900

    📑:: mics와 main 상호 연결

--- Fire-Gas/components/fire_core/core.c
+++ Fire-Gas/components/fire_core/core.c
@@ -1,5 +1,6 @@
 #include "core.h"
 #include "gpio.h"
+#include "mics.h"
 
 #include "esp_log.h"
 #include "driver/i2c.h"
@@ -8,11 +9,11 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-
 void MainCore(void* pvParameters) {
     xTaskCreate(sensor_check, "sensor_check", 6144, NULL, 5, NULL);
+    xTaskCreate(mics_sensor_get_value, "mics_sensor_get_value", 6144, NULL, 5, NULL);
 
     while (1) {
-
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
