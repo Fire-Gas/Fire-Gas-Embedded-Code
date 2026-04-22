a0e13e2daafb107b56b3581c2fb5750f4196e6e6
ilfpns <yeomsh09@gmail.com>
2026-04-22 22:18:11 +0900

    ⚡️:: 현재 상태 분류 LED 로직 추가

--- Fire-Gas/components/fire_common/include/common_struct.h
+++ Fire-Gas/components/fire_common/include/common_struct.h
@@ -24,4 +24,13 @@ typedef struct __attribute__((packed)) sensor_target_t{
     const char* name;
 } sensor_target_t;
 
+typedef enum final_value {
+    A = 1,
+    B = 2,
+    C = 3,
+    K = 4,
+    battery = 5,
+    warn = 6,
+} final_value;
+
 #endif
