b2653c6c446ceea2fb02bb385e56e890a2f0f67e
ilfpns <yeomsh09@gmail.com>
2026-04-22 21:56:36 +0900

    ⚡️:: LED 제어 함수 및 LED 메인 태스크 추가

--- Fire-Gas/components/fire_led/include/led.h
+++ Fire-Gas/components/fire_led/include/led.h
@@ -1,6 +1,11 @@
 #ifndef LED_INDICATE
 #define LED_INDICATE
 
-
+/**
+ * @brief led 제어 태스크
+ * @param void *pvParameters
+ * @return None
+ */
+void led_test_task(void *pvParameters);
 
 #endif
--- Fire-Gas/components/fire_led/led.c
+++ Fire-Gas/components/fire_led/led.c
@@ -3,6 +3,9 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 static const char* TAG = "LED";
 static const uint8_t LED_CNT = 5;
 static const gpio_num_t led_pins[5] = {
@@ -30,4 +33,36 @@ static void led_init(void) {
 
     gpio_config(&io_conf);
     ESP_LOGI(TAG, "%d개의 LED 초기화 완료", LED_CNT);
+}
+
+// 모든 LED 켜기 (사용하지 않음)
+static void set_led_state(int index, uint32_t state) {
+    if (index >= 0 && index < LED_CNT) {
+        gpio_set_level(led_pins[index], state);
+    }
+}
+// 모든 LED 끄기 (사용하지 않음)
+static void all_leds_off() {
+    for (int i = 0; i < LED_CNT; i++) {
+        gpio_set_level(led_pins[i], 0);
+    }
+}
+
+void led_test_task(void *pvParameters) {
+    led_init();
+
+    while (1) {
+        for (int i = 0; i < LED_CNT; i++) {
+            set_led_state(i, 1);
+            ESP_LOGI(TAG, "LED %d ON", i);
+            vTaskDelay(pdMS_TO_TICKS(200));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        all_leds_off();
+        ESP_LOGI(TAG, "All LEDs OFF");
+        
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
