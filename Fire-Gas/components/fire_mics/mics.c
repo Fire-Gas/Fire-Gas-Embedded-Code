54380e8e0f870cebcb6a620d7f671785273a6f06
ilfpns <yeomsh09@gmail.com>
2026-04-23 21:57:49 +0900

    ♻️:: 기존 MICS의 변수 반복 선언 문제 해결

--- Fire-Gas/components/fire_mics/mics.c
+++ Fire-Gas/components/fire_mics/mics.c
@@ -15,31 +15,29 @@
 static const char* TAG = "MICS";
 
 void mics_sensor_get_value(void* pvParameters) {
-    QueueHandle_t mics_queue_hanlder = (QueueHandle_t)pvParameters;
+    QueueHandle_t mics_queue_handler = (QueueHandle_t)pvParameters;
     mics_data_t data;
 
-    int co_raw, nh3_raw, no2_raw;
-    int co_mv, nh3_mv, no2_mv; // 위 변수 3개와 너무 동일함 -> 개선 필요
+    adc_channel_t channels[3] = {CO_CHANNEL, NH3_CHANNEL, NO2_CHANNEL};
+    int raw_vals[3];
+    int mv_vals[3];
 
     while(1) {
-        adc_oneshot_read(adc1_handle, CO_CHANNEL, &co_raw);
-        adc_oneshot_read(adc1_handle, NH3_CHANNEL, &nh3_raw);
-        adc_oneshot_read(adc1_handle, NO2_CHANNEL, &no2_raw);
-
-        // 2. 보정된 전압(mV)으로 변환
-        if (adc1_cali_handle) {
-            adc_cali_raw_to_voltage(adc1_cali_handle, co_raw, &co_mv);
-            adc_cali_raw_to_voltage(adc1_cali_handle, nh3_raw, &nh3_mv);
-            adc_cali_raw_to_voltage(adc1_cali_handle, no2_raw, &no2_mv);
-        } else {
-            co_mv = (co_raw * 3300) / 4095;
+        for (int i = 0; i < 3; i++) {
+            adc_oneshot_read(adc1_handle, channels[i], &raw_vals[i]);
+            
+            if (adc1_cali_handle) {
+                adc_cali_raw_to_voltage(adc1_cali_handle, raw_vals[i], &mv_vals[i]);
+            } else {
+                mv_vals[i] = (raw_vals[i] * 3300) / 4095;
+            }
         }
 
-        data.co = co_mv;
-        data.nh = nh3_mv;
-        data.no = no2_mv;
+        data.co = mv_vals[0];
+        data.nh = mv_vals[1];
+        data.no = mv_vals[2];
 
-        if (xQueueSend(mics_queue_hanlder, &data, pdMS_TO_TICKS(100)) != pdPASS) {
+        if (xQueueSend(mics_queue_handler, &data, pdMS_TO_TICKS(100)) != pdPASS) {
             ESP_LOGE(TAG, "mics data전송 실패, 재전송 시도");
         }
 
